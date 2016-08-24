0a97d1cd1d06aca1f9c6290a3596eae1819cf9d9
Corey Beres <corey.beres@gmail.com>
2016-08-24 14:34:05 -0400

    disambiguate predicted winners

--- app/game-detail.component.ts
+++ app/game-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Input } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 
 import { APP_CONFIG, AppConfig } from './app.config';
 import { FacebookShareComponent } from './facebook-share.component';
@@ -10,8 +10,26 @@ import { Game } from './game';
   styleUrls: ['app/game-detail.component.css'],
   directives: [FacebookShareComponent],
 })
-export class GameDetailComponent {
+export class GameDetailComponent implements OnInit {
   @Input() game: Game;
+  includeScore: boolean;
+  displayHomePred: number;
+  displayAwayPred: number;
 
   constructor(@Inject(APP_CONFIG) private config: AppConfig) {}
+
+  ngOnInit() {
+    this.includeScore = this.game.status == 'F';
+
+    // if rounding will suggest a tie, then add +/- 1 to one team's predicted score
+    let tiePredicted = Math.round(this.game.prediction.away_score) == Math.round(this.game.prediction.home_score);
+    let roundedTotal = Math.round(this.game.prediction.away_score) + Math.round(this.game.prediction.home_score);
+    let exactTotal = this.game.prediction.away_score + this.game.prediction.home_score;
+    let tieOffset = exactTotal > roundedTotal ? 1 : -1;
+    let predictedAwayWins = this.game.prediction.away_score > this.game.prediction.home_score;
+    let awayOffset = tiePredicted && ((predictedAwayWins && tieOffset > 0) || (!predictedAwayWins && tieOffset < 0)) ? tieOffset : 0;
+    let homeOffset = tiePredicted && ((predictedAwayWins && tieOffset < 0) || (!predictedAwayWins && tieOffset > 0)) ? tieOffset : 0;
+    this.displayAwayPred = this.game.prediction.away_score + awayOffset;
+    this.displayHomePred = this.game.prediction.home_score + homeOffset;
+  }
 }
